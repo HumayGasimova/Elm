e8ef26e06c3ab35f0a6624534b41231891bc4c08
Humay <crypto347@gmail.com>
2020-03-04 11:42:50 +0400

    ifStatment

---
+++ src/ifStatment.elm
@@ -0,0 +1,16 @@
+module Main exposing (..)
+import Html exposing (..)
+
+-- MAIN
+escapeEarth myVelocity mySpeed =
+    if myVelocity > 11.186 then
+        "Godspeed"
+
+    else if mySpeed == 7.67 then
+        "Stay in orbit"
+
+    else
+        "Come back"
+main = 
+  Html.text (escapeEarth 11.0 7.67)
+   
