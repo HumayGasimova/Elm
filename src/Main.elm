60549cee44923ba084f054e20ff1974974da38d9
Humay <crypto347@gmail.com>
2020-03-06 10:09:18 +0400

    string length

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,13 @@
 module Main exposing (..)
 import Browser
 import Html exposing (..)
+import String exposing (length)
 
 -- MAIN
 
-revelation =
+revelation str =
+    length str
 
--- multiple line
-    -- """
-    -- It became very clear to me sitting out there today
-    -- that every decision I've made in my entire life has
-    -- been wrong. My life is the complete "opposite" of
-    -- everything I want it to be. Every instinct I have,
-    -- in every aspect of life, be it something to wear,
-    -- something to eat - it's all been wrong.
-    -- """
--- single line
-     "Michael Scott's '\\' Rabies Awareness \"Fun Run\" Race for the Cure"
 main =
-   Html.text revelation
+   Html.text <| String.fromInt <| revelation "revelation"
    
