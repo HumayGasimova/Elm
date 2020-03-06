b1992a6e66535e501acd1e0dd0e7e5481272804c
Humay <crypto347@gmail.com>
2020-03-06 11:14:15 +0400

    anonymous function

--- src/stringFilter.elm
+++ src/stringFilter.elm
@@ -9,5 +9,7 @@ isValid char =
     char /= '-'
 
 main =
-   Html.text <| String.filter isValid "222-11-5555"
+--  Html.text <| String.filter isValid "222-11-5555"
+-- anonymous function
+    Html.text <| String.filter (\char -> char /= '-') "222-11-5555"
    
