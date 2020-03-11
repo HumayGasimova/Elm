e9bcab20c68b23c38f3c36fa423d010d3c8fc29b
Humay <crypto347@gmail.com>
2020-03-11 10:12:22 +0400

    take and drop list

--- src/Main.elm
+++ src/Main.elm
@@ -4,25 +4,11 @@ import Html exposing (..)
 import String exposing (..)
 
 -- MAIN
--- sum = List.foldl (\x a -> x + a) 0 [ 1, 2, 3, 4 ]
+lastEl = List.drop 2 [ "Smeagol", "Freddy", "Daenerys", "Jacob" ]
 
-sum = List.foldl (+) 0 [ 1, 2, 3, 4 ]
+firsEl = List.take 2 [ "Freddy", "Daenerys", "Driver" ]
 
--- product = List.foldl (\x a -> x * a) 1 [ 1, 2, 3, 4 ]
-
-product = List.foldl (*) 1 [ 1, 2, 3, 4 ]
-   
-guardians = [ "Star-lord", "Groot", "Gamora", "Drax", "Rocket" ]
-
-guardiansLength = List.foldl (\x a -> (String.length x) + a) 0 guardians
-
---Folding a List from Right
-
-foldFromLeft = List.foldl (\x a -> x ^ a) 1 [ 4, 2, 3 ]
-
-foldFromRight = List.foldr (\x a -> x ^ a) 1 [ 4, 2, 3 ]
-
-main = foldFromLeft
+main = firsEl
         |> Debug.toString
         |> Html.text
         
