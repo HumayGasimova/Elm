6dee9612eeb830c6c2ae7b4f31b2ace1537ed996
Humay <crypto347@gmail.com>
2020-03-11 09:49:26 +0400

    folding list from right

--- src/Main.elm
+++ src/Main.elm
@@ -15,7 +15,14 @@ product = List.foldl (*) 1 [ 1, 2, 3, 4 ]
 guardians = [ "Star-lord", "Groot", "Gamora", "Drax", "Rocket" ]
 
 guardiansLength = List.foldl (\x a -> (String.length x) + a) 0 guardians
-main = guardiansLength
+
+--Folding a List from Right
+
+foldFromLeft = List.foldl (\x a -> x ^ a) 1 [ 4, 2, 3 ]
+
+foldFromRight = List.foldr (\x a -> x ^ a) 1 [ 4, 2, 3 ]
+
+main = foldFromLeft
         |> Debug.toString
         |> Html.text
         
