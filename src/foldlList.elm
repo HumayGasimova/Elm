0cdc82afaed217ab30b5586d737f1221fd9a93fb
Humay <crypto347@gmail.com>
2020-03-11 09:25:35 +0400

    folding list updated

--- src/foldlList.elm
+++ src/foldlList.elm
@@ -12,7 +12,10 @@ sum = List.foldl (+) 0 [ 1, 2, 3, 4 ]
 
 product = List.foldl (*) 1 [ 1, 2, 3, 4 ]
    
-main = product
+guardians = [ "Star-lord", "Groot", "Gamora", "Drax", "Rocket" ]
+
+guardiansLength = List.foldl (\x a -> (String.length x) + a) 0 guardians
+main = guardiansLength
         |> Debug.toString
         |> Html.text
         
